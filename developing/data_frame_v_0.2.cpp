9c35e1c16c2334540e2dd9c13806b87f79578cb5
Aperjump <wei@aperjump.me>
2019-05-31 22:58:51 -0700

    update on data_frame_v_0.2.cpp, able to print row message, but cannot add it at run-time

--- developing/data_frame_v_0.2.cpp
+++ developing/data_frame_v_0.2.cpp
@@ -4,6 +4,8 @@
 #include <memory>
 #include <typeinfo>
 #include <algorithm>
+#include <iostream>
+#include <tuple>
 class col_base {
 
 };
@@ -28,9 +30,10 @@ public:
     template<typename T>
     void add_cols(std::string col_name, col<T>& col_vector) {
         if (_m.size() == 0) {
-            boost::numeric::ublas::vector<int> index(col_vector._store.size());
-            for (int i = 0; i < col_vector._store.size(); i++)
+            index = boost::numeric::ublas::vector<int>(col_vector._store.size());
+            for (int i = 0; i < col_vector._store.size(); i++) {
                 index[i] = i;
+            }
         }
         col_base* tmp = &col_vector;
         _type_map.insert({col_name, typeid(T).name()});
@@ -41,8 +44,9 @@ public:
         if (!_type_map.count(col_name)) return {};
         if (typeid(T).name() != _type_map[col_name]) return {};
         std::vector<int> tmp_index;
-        for (int i = 0; i < index.size(); i++)
+        for (int i = 0; i < index.size(); i++) {
             tmp_index.push_back(index[i]);
+        }
         col<T>* new_col = reinterpret_cast<col<T>*>(_m[col_name]);
         auto cmp = [&](int& l, int& r) -> bool {
             return new_col->_store[l] > new_col->_store[r];
@@ -50,6 +54,36 @@ public:
         std::sort(tmp_index.begin(), tmp_index.end(), cmp);
         return tmp_index;
     }
+    /* The problem with this design approach is that it will make it hard to implement operations
+    like join, since in template arguments, we can only specify the type of join on column, but cannot have 
+    any information for other columns
+    Another problem is when adding multiple rows, we need to provide information for each column. 
+    */
+    template<class Tuple, std::size_t N>
+    struct some_class {
+        static void print(const Tuple& t, std::vector<std::string>& names) 
+        {
+            some_class<Tuple, N-1>::print(t, names);
+            std::cout << ", " << names[N-1] << ": "<< std::get<N-1>(t);
+        }
+    };
+    
+    template<class Tuple>
+    struct some_class<Tuple, 1> {
+        static void print(const Tuple& t, std::vector<std::string>& names) 
+        {
+            std::cout << names[0] << ": " << std::get<0>(t);
+        }
+    };
+    
+    template<class... Args>
+    void print_row(const std::tuple<Args...>& t, std::vector<std::string>& names) 
+    {
+        std::cout << "(";
+        some_class<decltype(t), sizeof...(Args)>::print(t, names);
+        std::cout << ")\n";
+    }
+    
 private:
     boost::numeric::ublas::vector<int> index;
     std::map<std::string, col_base*> _m;
@@ -65,13 +99,24 @@ int main() {
     for (int i = 0; i < 10; i++)
         double_vec[i] = i * 2.0;
     col<double> double_cl("double_vec", double_vec);  
+    
     boost::numeric::ublas::vector<std::string> string_vec(10);
     for (int i = 0; i < 10; i++)
         string_vec[i] = std::to_string(i);
-    col<std::string> string_cl("double_vec", string_vec);  
+    col<std::string> string_cl("string_vec", string_vec);  
+    
     data_frame df;
     df.add_cols<int>(int_cl.get_name(), int_cl);
     df.add_cols<double>(double_cl.get_name(), double_cl);
     df.add_cols<std::string>(string_cl.get_name(), string_cl);
     std::vector<int> order = df.sort<double>("double_vec");
+    for (int i = 0; i < order.size(); i++)
+        std::cout << std::to_string(order[i]) << std::endl;
+    std::cout << "-----" << std::endl;
+    std::vector<int> order2 = df.sort<std::string>("string_vec");
+    for (int i = 0; i < order2.size(); i++)
+        std::cout << std::to_string(order2[i]) << std::endl;
+    std::tuple<int, std::string, float> t1(10, "Test", 3.14);
+    std::vector<std::string> tmp({"int_vec", "string_vec", "double_vec"});
+    df.print_row(t1, tmp);
 }
