11c4a46e349ac21be3c32c7635864d4147a5d807
Aperjump <wei@aperjump.me>
2019-06-23 14:37:24 -0700

    Add support for GTest

--- test/data_frame_col_test.cpp
+++ test/data_frame_col_test.cpp
@@ -1,19 +1,18 @@
 #include <boost/numeric/ublas/vector.hpp>
 #include "data_frame_col.hpp"
+#include "gtest/gtest.h"
 #include <vector>
 #include <iostream>
 using namespace boost::numeric::ublas;
 /* will be replaced by unit test frame work */
-int main() {
+
+TEST(Data_frame_col, initialize) {
     data_frame_col df;
     std::vector<int> tmp = {1, 2, 3, 4, 5};
     df.build_by_vec("int_vec", tmp);
     int len = df.get_size<int>();
+    EXPECT_EQ(5, len);
     for (int i = 0; i < len; i++) {
-        std::cout << df.at<int>(i) << std::endl;
-    }
-    data_frame_col df2("int_vec", tmp);
-    for (int i = 0; i < len; i++) {
-        std::cout << df2.at<int>(i) << std::endl;
+        EXPECT_EQ(i + 1, df.at<int>(i));
     }
 }
